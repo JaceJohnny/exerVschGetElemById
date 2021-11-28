fc2af87097efce64123627388014cbbee5bcad26
JaceJohnny <jace.johnny@hci.utah.edu>
2021-11-27 20:27:47 -0700

    Completed Elements by ID assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+var headline = document.getElementById("headline")
+console.log(headline)
+
+headline.textContent += "This is the headline to my HTML";
+console.log("this is the added text", headline)
+
+
